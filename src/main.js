f3371cc210b98b23666f4de5fba42dfd900e38d0
Alan Rehfeldt <alanrehfeldt@gmail.com>
2022-10-20 12:09:17 -0300

    Second class, importing IMask and creating masks with regex for validating card

--- src/main.js
+++ src/main.js
@@ -1,21 +1,74 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-
 function setCardType(type) {
-    const colors = {
-        visa: ["#436D99", "#2D57F2"], 
-        mastercard: ["#DF6F29", "#C69347"],
-        default: ["black", "gray"]
-    }
-    
-    ccBgColor01.setAttribute("fill", colors[type][0])
-    ccBgColor02.setAttribute("fill", colors[type][1])
-    ccLogo.setAttribute("src", `cc-${type}.svg`)
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const experationData = document.querySelector("#expiration-date")
+const experationDataPattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+}
+const experationDataMasked = IMask(experationData, experationDataPattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cadtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cadtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cadtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMask) {
+    const number = (dynamicMask.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMask.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    return foundMask
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
